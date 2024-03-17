d89b857122f46cf2ec5a86b82d758ecd322eb692
Phillip <drphil@umich.edu>
2024-03-17 00:14:16 -0400

    Fixed indentation and comments

--- display_graph.py
+++ display_graph.py
@@ -1,36 +1,30 @@
-"""This module creates a line graph of Toronto's average
-mean temperatures, degrees Celsius, in 2017 and 2022"""
+"""
+This module creates a line graph of Toronto's average
+max and min temperatures in degrees Celsius, in 2017 and 2022
+"""
 import pandas as pd
 import altair as alt
 
 def create_chart(df):
     """
+    This function takes in a df and produces the line chart
     :param df: the dataframe of the data source
     :return: Altair chart
 
     """
 
-    # Define a dictionary for mapping month to color 
-    # [December, January, February = Winter] [March, April, May = Spring] [June, July, August = Summer] [September, October, November = Fall]
-    label_colors = {
-        'condition': [
-            {'test' : 'datum.label == "1"', 'value': 'black'},
-            {'test' : 'datum.label == "2"', 'value': 'black'},
-            {'test' : 'datum.label == "3"', 'value': 'green'},
-            {'test' : 'datum.label == "4"', 'value': 'green'},
-            {'test' : 'datum.label == "5"', 'value': 'green'},
-            {'test' : 'datum.label == "6"', 'value': 'blue'},
-            {'test' : 'datum.label == "7"', 'value': 'blue'},
-            {'test' : 'datum.label == "8"', 'value': 'blue'},
-            {'test' : 'datum.label == "9"', 'value': 'orange'},
-            {'test' : 'datum.label == "10"', 'value': 'orange'},
-            {'test' : 'datum.label == "11"', 'value': 'orange'}],
-        'value': 'black'}  # The default value if no condition is met (e.g. 12 December)
-
     # Generate a line plot in Altair showing the month and average mean temperatures
     base = alt.Chart(df).mark_line(point=True).encode(
-        x=alt.X("Month:O", title="Month", axis=alt.Axis(labelAngle=0, tickSize=8, labelFontSize=11, labelFontWeight=500,labelColor=label_colors)),
-        y=alt.Y("value", title="Temperature  (C)", axis=alt.Axis(tickSize=8, labelFontSize=11, labelFontWeight=500)),
+        x=alt.X(
+            "Month:O", 
+            title="Month", 
+            axis=alt.Axis(labelAngle=0, tickSize=8, labelFontSize=11, labelFontWeight=500)
+        ),
+        y=alt.Y(
+            "value", 
+            title="Temperature  (C)", 
+            axis=alt.Axis(tickSize=8, labelFontSize=11, labelFontWeight=500)
+        ),
         color=alt.Color("variable:N", title="Legend"),
         tooltip=["value:Q"], #info for the dots when user hovers there
     )
@@ -60,7 +54,7 @@ def create_chart(df):
     ).configure_axis(
         grid = False
     ).configure_legend(
-        labelFontSize=12   # Adjust legend label font size
+        labelFontSize=12   
     )
  
     return chart
